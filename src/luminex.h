f5c3397693d4929268bc88f4e24563faceeb0af9
changcheng967 <changcheng967@users.noreply.github.com>
2026-01-29 18:24:11 -0500

    bump version to 2.5.0

--- src/luminex.h
+++ src/luminex.h
@@ -16,7 +16,7 @@ namespace luminex {
 // Engine info
 constexpr const char* ENGINE_NAME = "Luminex";
 constexpr const char* ENGINE_AUTHOR = "changcheng967";
-constexpr const char* ENGINE_VERSION = "2.4.0";
+constexpr const char* ENGINE_VERSION = "2.5.0";
 
 // Initialize engine
 void init();
