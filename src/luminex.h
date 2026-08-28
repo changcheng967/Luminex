72243055a8118789afe76ad673e65b29a94b37e2
changcheng967 <changcheng967@users.noreply.github.com>
2026-08-28 15:26:45 -0400

    Bump version to 5.16.0
    Release version for the fit6 distilled evaluation and the 256 MB
    transposition table default. Syncs src/luminex.h and CMakeLists.txt.

--- src/luminex.h
+++ src/luminex.h
@@ -16,7 +16,7 @@ namespace luminex {
 // Engine info
 constexpr const char* ENGINE_NAME = "Luminex";
 constexpr const char* ENGINE_AUTHOR = "changcheng967";
-constexpr const char* ENGINE_VERSION = "5.15.1-dev";
+constexpr const char* ENGINE_VERSION = "5.16.0";
 
 // Initialize engine
 void init();
